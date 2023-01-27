378d5375a28be44c58aa032ddcae203831ba1879
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-01-27 14:35:24 +0530

    Update point.java

--- src/main/java/je/point.java
+++ src/main/java/je/point.java
@@ -2,7 +2,7 @@ package je;
 
 import java.util.*;
 
-class point1 implements Cloneable {
+class POINT1 implements Cloneable {
     float x1;
     float x2;
     float y1;
@@ -27,7 +27,7 @@ class point1 implements Cloneable {
     
 }
 
-public class point{
+public class POINT{
     public static void main(String[] args) throws CloneNotSupportedException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter x1 and y1:");
@@ -37,9 +37,9 @@ public class point{
         float x2 = sc.nextFloat();
         float y2 = sc.nextFloat();
         sc.close();
-        point1 obj1 = new point1(x1, x2, y1, y2);
+        POINT1 obj1 = new POINT1(x1, x2, y1, y2);
         System.out.println("Point1 equals Point2: "+obj1.equals());
-        point1 obj2 = (point1)obj1.clone();
+        POINT1 obj2 = (POINT1)obj1.clone();
         System.out.println("Original Point (x1,y1) "+obj1.x1+" "+obj1.y1+" (x2,y2) "+obj1.x2+" "+obj1.y2);
         System.out.println("After Cloning (x1 ,y1) "+obj2.x1+" "+obj2.y1+" (x2,y2) "+obj2.x2+" "+obj2.y2);
     }
